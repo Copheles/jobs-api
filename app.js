17934fa877d647a70ce07526d30aae25ea305fc3
Copheles <xixxykitkit@gmail.com>
2022-08-01 07:24:47 +0630

    swagger docs added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
+
+
 const express = require("express");
 const app = express();
 
@@ -24,6 +26,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger 
+
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 app.set('trust proxy', 1);
 app.use(rateLimiter({
 	windowMs: 15 * 60 * 1000, // 15 mins
@@ -34,10 +42,12 @@ app.use(cors());
 app.use(xss());
 
 
+
 app.get('/', (req, res) => {
-  res.send('jobs api')
+  res.send('<h1>Jobs Api</h1> <a href="/api-docs">Documentation</a>')
 })
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser,  jobsRouter);
